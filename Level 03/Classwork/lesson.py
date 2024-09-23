2292e6e23a0090ec6a8c619bb1bafaa5dd6e8012
valishvili2015 <valishvili2015@gmail.com>
2024-09-23 22:04:38 +0400

    GOA

---
+++ Level 03/Classwork/lesson.py
@@ -0,0 +1,1 @@
+name = "alexandre"
---
+++ homework.py
@@ -0,0 +1,17 @@
+a = 5
+b = 7
+# result = შედეგი 
+
+result = a + b
+
+
+age = 9 
+wheight = 40
+height = 130
+print(age, wheight, height)
+
+movie = "the lion king"
+
+int = 100000
+float = 1.5
+string = "monkey"
